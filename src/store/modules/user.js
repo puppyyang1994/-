1db81c3e78edf623c3b856bf7d9a3a711de9691e
puppyyang1994 <2472284160@qq.com>
2023-03-17 14:38:07 +0800

    项目初始化

---
+++ src/store/modules/user.js
@@ -0,0 +1,11 @@
+/* import { login, logout, getInfo } from '@/api/user'
+import { getToken, setToken, removeToken } from '@/utils/auth'
+import { resetRouter } from '@/router' */
+
+export default {
+  namespaced: true,
+  state: {},
+  mutations: {},
+  actions: {}
+}
+
